cfeb1051480dc9475e732e03a6d7a05971ea529f
rais ilham <rais.ilham.nustara-2020@fst.unair.ac.id>
2021-08-01 17:11:00 +0700

    Add solution for problem `A - Elephant` Link: https://codeforces.com/contest/617/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/617/submission/124496411

---
+++ codeforces/617/A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a, pas;
+    cin >> a;
+    pas = a / 5;
+    if (a % 5 != 0)
+    {
+        pas++;
+    }
+    cout << pas << endl;
+}
