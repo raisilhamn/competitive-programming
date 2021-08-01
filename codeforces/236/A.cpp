8a537f6f667796793d3dd07873239e20143394e7
rais ilham <rais.ilham.nustara-2020@fst.unair.ac.id>
2021-08-01 10:00:00 +0700

    Add solution for problem `A - Boy or Girl` Link: https://codeforces.com/contest/236/problem/A Tags: brute force, implementation, strings, *800 Ref: https://codeforces.com/contest/236/submission/124468439

--- codeforces/236/A.cpp
+++ codeforces/236/A.cpp
@@ -6,28 +6,29 @@ int main()
     string str;
     int jumlah, pass = 1;
     cin >> str;
-    for (int i = 0; i < str.size(); i++)
-    {
-        for (int j = 0; j < i; j++)
-        {
-            if (str[i] == str[j])
-            {
-                jumlah++;
-            }
-        }
-        if (jumlah == 0)
-        {
-            pass++;
-        }
-        jumlah = 0;
-    }
+   for (int i = 0; i < str.size(); i++)
+   {
+       for (int j = 0; j < i ; j++)
+       {
+           if (str[i] == str[j])
+           {
+               jumlah++;
+           }
+           
+       }
+       if (jumlah == 0)
+       {
+           pass++;
+       }
+       jumlah = 0;
+   }
 
-    if (pass % 2 == 0)
-    {
-        cout << "IGNORE HIM!" << endl;
-    }
-    else
-    {
-        cout << "CHAT WITH HER!" << endl;
-    }
+   if (pass % 2 == 0)
+   {
+       cout << "IGNORE HIM!" << endl;
+   }
+   else
+   {
+       cout << "CHAT WITH HER!" << endl;
+   }
 }
