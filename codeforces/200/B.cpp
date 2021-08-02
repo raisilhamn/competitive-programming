d78f092ab1080101581072395bd2d683de5efa01
rais ilham <rais.ilham.nustara-2020@fst.unair.ac.id>
2021-08-02 16:25:00 +0700

    Add solution for problem `B - Drinks` Link: https://codeforces.com/contest/200/problem/B Tags: implementation, math, *800 Ref: https://codeforces.com/contest/200/submission/124670070

--- codeforces/200/B.cpp
+++ codeforces/200/B.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 int main()
 {
-    int n, drink;
+    int n, a[110];
     cin >> n;
     double ans = 0.0, sum = 0.0;
     for (int i = 0; i < n; i++)
     {
-        cin >> drink;
-        sum += drink;
+        cin >> a[i];
+        sum += a[i];
     }
     ans = sum / n;
     cout << fixed << setprecision(12) << ans << endl;
