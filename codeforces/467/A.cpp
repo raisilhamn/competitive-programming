845da42ada35b30439784e4d9b50b2cc40c5835a
rais ilham <rais.ilham.nustara-2020@fst.unair.ac.id>
2021-08-03 17:30:00 +0700

    Add solution for problem `A - George and Accommodation` Link: https://codeforces.com/contest/467/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/467/submission/124761708

---
+++ codeforces/467/A.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int people, room, move(0);
+    int n;
+    cin >> n;
+    for (size_t i = 0; i < n; i++)
+    {
+        cin >> people >> room;
+        if ((room - people) >= 2)
+        {
+            move++;
+        }
+    }
+    cout << move << endl;
+
+    return 0;
+}
