b4905fa351e2b184a10918219c7183f09b9a559e
rais ilham <rais.ilham.nustara-2020@fst.unair.ac.id>
2021-08-04 09:13:00 +0700

    Add solution for problem `A - Anton and Letters` Link: https://codeforces.com/contest/443/problem/A Tags: constructive algorithms, implementation, *800 Ref: https://codeforces.com/contest/443/submission/124810227

---
+++ codeforces/443/A.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+#define endl "\n"
+#define MOD 1000000007
+
+/*
+    isalpha(type n) ;
+    Check if character is alphabetic
+    https://www.cplusplus.com/reference/cctype/isalpha/
+
+*/
+
+using namespace std;
+
+int main()
+{
+    fastio;
+    string s;
+    set<char> l;
+    getline(cin, s);
+
+    int i = 0;
+    while (s[i])
+    {
+        if (isalpha(s[i])) // isalpha  : Check if character is alphabetic
+            l.insert(s[i]);
+        i++;
+    }
+
+    cout << l.size() << endl;
+    // cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << "\n";
+    return 0;
+}
