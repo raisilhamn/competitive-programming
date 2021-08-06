2514babe6f2346eb2ca7532f5c29848ed821d03d
rais ilham <rais.ilham.nustara-2020@fst.unair.ac.id>
2021-08-06 21:14:00 +0700

    Add solution for problem `B - Balanced Array` Link: https://codeforces.com/contest/1343/problem/B Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1343/submission/125061204

---
+++ codeforces/1343/B.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+#define endl "\n"
+#define MOD 1000000007
+#define ll long long
+/* notes
+if there are multiple answers, you can print any. 
+It is not guaranteed that the answer exists.
+
+
+*/
+
+using namespace std;
+
+int main()
+{
+    fastio;
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        n /= 2;
+        if (n & 1)
+        {
+            cout << "NO" << endl;
+            continue; // end this loop
+        }
+        // if not even 
+        cout << "YES" << endl;
+        for (int i = 1; i <= n; i++) // Pre-increment, ditambah dulu baru 
+        {
+            cout << i * 2 << " ";
+        }
+        for (int i = 1; i < n; i++) // Pre-increment
+        {
+            cout << i * 2 - 1 << " ";
+        }
+        cout << 3 * n - 1 << endl; // akhir dari deret, sedikit modifikasi
+        // 
+    }
+
+    return 0;
+}
