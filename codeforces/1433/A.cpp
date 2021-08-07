610330158116af22f08c75226b75f8b702896aa3
rais ilham <rais.ilham.nustara-2020@fst.unair.ac.id>
2021-08-08 06:18:00 +0700

    Add solution for problem `A - Boring Apartments` Link: https://codeforces.com/contest/1433/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/1433/submission/125175037

--- codeforces/1433/A.cpp
+++ codeforces/1433/A.cpp
@@ -25,18 +25,11 @@ int main()
     
     while(t--)
     {
-
-        int x, d = 0;
-        cin >> x;
-        int a = x % 10;
-
-        string s = to_string(x);
-        for (int i = 0; i < s.size(); i++)
-        {
-            d += 1 + i;
-        }
-
-        cout << ((a - 1) * 10) + d << endl;
+        string num;
+        cin >> num;
+        int dig = num[0] - '0' - 1;
+        int len = num.size();
+        cout << dig * 10 + len * (len + 1) / 2 << endl;
     }
     return 0;
 }
